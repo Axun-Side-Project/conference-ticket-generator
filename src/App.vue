7dd7787a7306ff3e46b86346394db49392f6eba4
阿循 <vincentgan0402@gmail.com>
2025-01-05 19:53:13 +0800

    init: 初始化環境

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  你好，世界！
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+}
+</script>
+
+<style style="scss" scoped></style>
---
+++ src/store/actions.js
@@ -0,0 +1,14 @@
+export default {
+
+	/**
+	 * Helper Function
+	 * 
+	 * These functions are designed for internal purposes 
+	 * and are not exposed for direct client usage.
+	 */
+
+	/**
+	 * Client Function 
+	 */
+
+}
